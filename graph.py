79e401418bf758f365f9ab1d53c0a87462ed00f3
Srivignessh Pacham <srivignessh@appformix.com>
2018-10-28 00:14:27 -0700

    Implement Basic Graph Class

--- graph.py
+++ graph.py
@@ -1,103 +1,53 @@
-class Graph(object):
-
-    def __init__(self, graph_dict=None):
-        """ initializes a graph object 
-            If no dictionary or None is given, 
-            an empty dictionary will be used
-        """
-        if graph_dict == None:
-            graph_dict = {}
-        self.__graph_dict = graph_dict
-
-    def vertices(self):
-        """ returns the vertices of a graph """
-        return list(self.__graph_dict.keys())
-
-    def edges(self):
-        """ returns the edges of a graph """
-        return self.__generate_edges()
-
-    def add_vertex(self, vertex):
-        """ If the vertex "vertex" is not in 
-            self.__graph_dict, a key "vertex" with an empty
-            list as a value is added to the dictionary. 
-            Otherwise nothing has to be done. 
-        """
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = []
-
-    def add_edge(self, edge):
-        """ assumes that edge is of type set, tuple or list; 
-            between two vertices can be multiple edges! 
-        """
-        edge = set(edge)
-        (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
-        else:
-            self.__graph_dict[vertex1] = [vertex2]
-
-    def __generate_edges(self):
-        """ A static method generating the edges of the 
-            graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
-        """
-        edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
-
-    def __str__(self):
-        res = "vertices: "
-        for k in self.__graph_dict:
-            res += str(k) + " "
-        res += "\nedges: "
-        for edge in self.__generate_edges():
-            res += str(edge) + " "
-        return res
-
-
-if __name__ == "__main__":
-
-    g = { "a" : ["d"],
-          "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
-          "e" : ["c"],
-          "f" : []
-        }
-
-
-    graph = Graph(g)
-
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Edges of graph:")
-    print(graph.edges())
-
-    print("Add vertex:")
-    graph.add_vertex("z")
-
-    print("Vertices of graph:")
-    print(graph.vertices())
+from collections import defaultdict
+from collections import namedtuple
  
-    print("Add an edge:")
-    graph.add_edge({"a","z"})
-    
-    print("Vertices of graph:")
-    print(graph.vertices())
+Edge = namedtuple('Edge', ('source', 'target', 'weight'))
+ 
+class Graph:
+    def __init__(self, vertices, edges=tuple()):
+        self.vertices = vertices
+        self.incident_edges = defaultdict(list)
+ 
+        for edge in edges:
+            self.add_edge(edge[0], edge[1], 1 if len(edge) == 2 else edge[2])
+ 
+    def add_edge(self, u, v, weight=1):
+        self.incident_edges[u].append(Edge(u, v, weight))
+        self.incident_edges[v].append(Edge(v, u, weight))
+ 
+    def edge(self, u, v):
+        return [e for e in self.incident_edges[u] if e.target == v][0]
 
-    print("Edges of graph:")
-    print(graph.edges())
+'''
+Graph 
+ 
+    a       k
+     b     j
+      cfghi
+     d     l
+    e       m
+'''
+G = Graph(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+           'j', 'k', 'l', 'm'],
+          [
+               ('a', 'b'),
+               ('b', 'c'),
+               ('c', 'd'),
+               ('d', 'e'),
+               ('c', 'f'),
+               ('f', 'g'),
+               ('g', 'h'),
+               ('h', 'i'),
+               ('i', 'j'),
+               ('j', 'k'),
+               ('i', 'l'),
+               ('l', 'm'),
+          ])
+print("Vertices of graph:")
+print(G.vertices)
 
-    print('Adding an edge {"x","y"} with new vertices:')
-    graph.add_edge({"x","y"})
-    print("Vertices of graph:")
-    print(graph.vertices())
-    print("Edges of graph:")
-    print(graph.edges())
-  
+print("Incident Edges:")
+print(G.incident_edges)
+
+print("Edge b -> c")
+print(G.edge('b','c'))
